41e220a39b32ce01f6385e6609f48fb861da7c13
toddhalfpenny <toddhalfpenny@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-20 16:42:54 +0000

    First commit of widgets-on-pages... how exciting!
    git-svn-id: http://plugins.svn.wordpress.org/widgets-on-pages/trunk@231147 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Widgets on Pages ===
+Contributors: toddhalfpenny
+Donate link: n/a
+Tags: widgets, sidebar, pages, post
+Requires at least: 2.8
+Tested up to: 2.9.1
+Stable tag: 0.0.1
+
+Allows 'in-page' widget areas so widgets can be defined via shortcut straight into page/post content
+
+== Description ==
+
+Allows 'in-page' widget areas so widgets can be defined via shortcut straight into page/post content
+
+
+== Installation ==
+
+
+1. Upload the directory `widgets-on-pages` and all its contents to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Add the following lines to the end of your themes `functions.php` file
+`<?php if ( function_exists('register_sidebar') )
+register_sidebar(array(
+    'name' => 'Widgets on Pages',
+    'before_widget' => '<li id="%1$s" class="widget %2$s">',
+    'after_widget' => '</li>',
+    'before_title' => '<h2 class="widgettitle">',
+    'after_title' => '</h2>',
+)); ?>`
+1. Add the shortcut `[widgets_on_pages]` to the page or post in the place where you'd like your widgets to appear.
+1. Add the widgets you want to the `Widgets on Pages` widget area in the admin screens
+ 
+
+== Frequently Asked Questions ==
+
+= Can I have more than one defined sidebar area =
+
+No, sorry not yet. The plan is to get this supported though
+
+== Changelog ==
+
+= 0.0.1 = 
+
+1st release - only supports one defined in-post/page widget area
---
+++ widgets_on_pages.php
@@ -0,0 +1,52 @@
+<?php
+
+/*  Copyright 2010  TODD HALFPENNY  (email : todd@gingerbreaddesign.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+Plugin Name: Widgets on Pages
+Plugin URI: http://gingerbreaddesign.co.uk/wordpress/plugins/widgets_on_pages
+Description: Allows 'in-page' widget areas so widgets can be defined via shortcut straight into page/post content
+Author: Todd Halfpenny
+Version: 0.0.1
+Author URI: http://gingerbreaddesign.co.uk/todd
+*/
+
+
+/* ===============================
+  C O R E    C O D E 
+================================*/
+
+
+function widgets_on_page(){
+  $str =  "<div id='widgets_on_page'>
+    <ul>";
+  ob_start();
+  if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Widgets on Pages") ) :
+  endif;
+  $myStr = ob_get_contents();
+  ob_end_clean();
+  $str .= $myStr;
+  $str .=  "</ul>
+  </div><!-- widget_on_page -->";
+return $str;
+}
+
+add_shortcode('widgets_on_pages', 'widgets_on_page');
+
+
+?>
